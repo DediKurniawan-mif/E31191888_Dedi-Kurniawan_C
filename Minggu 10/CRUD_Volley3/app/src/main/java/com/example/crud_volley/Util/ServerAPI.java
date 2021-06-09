7cfa5ab56c38b894e15b13f5f9d71d4067008d24
DediKurniawan-mif <dedikurniawan.real@gmail.com>
2021-06-09 23:13:38 +0700

    Minggu 10

---
+++ Minggu 10/CRUD_Volley3/app/src/main/java/com/example/crud_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.crud_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.48.161/crud/Mahasiswa/ApiData";
+    public static final String URL_INSERT = "http://192.168.48.161/crud/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.48.161/crud/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.48.161/crud/Mahasiswa/ApiUpdate";
+}
